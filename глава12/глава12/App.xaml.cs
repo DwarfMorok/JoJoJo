438a9b9d25fa9c8e5b4e6edefd52da2c8b5e87d7
DwarfMorok <162573539+DwarfMorok@users.noreply.github.com>
2025-02-26 14:47:15 +0300

    Add files via upload

---
+++ глава12/глава12/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Обработка параметров командной строки
+            if (e.Args.Length > 0)
+            {
+                MessageBox.Show("Запущено с параметром: " + e.Args[0]);
+            }
+
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            MessageBox.Show("Приложение завершается.");
+            base.OnExit(e);
+        }
+    }
+}
---
+++ глава12/глава12/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Загрузка изображения из GitHub
+            var imageUrl = "https://raw.githubusercontent.com/DwarfMorok/JoJoJo/main/JoJo.jpg";
+            var bitmap = new BitmapImage(new Uri(imageUrl));
+            ImageControl.Source = bitmap;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Закрытие приложения через класс Application
+            Application.Current.Shutdown();
+        }
+    }
+}
